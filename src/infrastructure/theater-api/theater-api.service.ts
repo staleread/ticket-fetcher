c8227522fab62310ad80e1950ae060feaa5365c0
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2024-07-18 01:16:05 +0300

    Fix bug within axios requests.
    The query params are not parsed

--- src/infrastructure/theater-api/theater-api.service.ts
+++ src/infrastructure/theater-api/theater-api.service.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Result, ErrorResult } from '../../common/exceptions/result';
+import axios from 'axios';
+import { ErrorResult, Result } from '../../common/exceptions/result';
 import { LoginInfo } from '../../config/login-info';
 import { PriceDto } from './dto/price.dto';
 import { SeatDto } from './dto/seat.dto';
@@ -23,28 +23,26 @@ export class TheaterApiService {
     this._httpClient = axios.create({
       baseURL: `https://${domain}/tessitura/ctglive`,
       timeout: 60_000,
-    })
+    });
   }
 
   async getPricesPerZone(eventId: number): Promise<Result<PriceDto[]>> {
-    const queryParams = {
-      id: TheaterApiService.REQUEST_ID,
-      method: 'GetPerformanceDetailWithDiscountingEx',
-    }
-
     const queryBlobParams = {
-      params: {
-        sSessionKey: this._loginInfo.sessionKey,
-        iPerf_no: eventId,
-        iModeofSale: this._loginInfo.modeOfSale,
-      }
-    }
-
-    const query = this._getQuery(queryParams, queryBlobParams);
+      SessionKey: this._loginInfo.sessionKey,
+      iPerf_no: eventId,
+      iModeOfSale: this._loginInfo.modeOfSale,
+    };
 
     try {
-      const responseBody = await this._httpClient.get('', { data: query });
-      return this._retrievePrices(responseBody);
+      const response = await this._httpClient.get('', {
+        params: {
+          id: TheaterApiService.REQUEST_ID,
+          method: 'GetPerformanceDetailWithDiscountingEx',
+          params: this._toBase64String(queryBlobParams),
+        },
+      });
+
+      return this._retrievePrices(response.data);
     } catch (err) {
       const errorResult: ErrorResult = this._resolveRequestError(err);
       return Result.fromFailure(errorResult);
@@ -52,29 +50,27 @@ export class TheaterApiService {
   }
 
   async getTheaterLayout(eventId: number): Promise<Result<TheaterLayoutDto>> {
-    const queryParams = {
-      id: TheaterApiService.REQUEST_ID,
-      method: 'GetSeatsBriefWithMOS',
-    }
-
     const queryBlobParams = {
-      params: {
-        sSessionKey: this._loginInfo.sessionKey, 
-        iModeOfSale: this._loginInfo.modeOfSale,
-        iSourceNumber: this._loginInfo.sourceNumber,
-        iPerformanceNumber: eventId,
-        iPackageNumber: 0,
-        cSummaryOnly: "N",
-        cCalcPackageAlloc: "N",
-        cReturnNonSeats: "N"
-      }
-    }
-
-    const query = this._getQuery(queryParams, queryBlobParams);
+      sSessionKey: this._loginInfo.sessionKey,
+      iModeOfSale: this._loginInfo.modeOfSale,
+      iSourceNumber: this._loginInfo.sourceNumber,
+      iPerformanceNumber: eventId,
+      iPackageNumber: 0,
+      cSummaryOnly: 'N',
+      cCalcPackageAlloc: 'Y',
+      cReturnNonSeats: 'Y',
+    };
 
     try {
-      const responseBody = await this._httpClient.get('', { data: query });
-      return this._retrieveTheaterLayout(responseBody);
+      const response = await this._httpClient.get('', {
+        params: {
+          id: TheaterApiService.REQUEST_ID,
+          method: 'GetSeatsBriefWithMOS',
+          params: this._toBase64String(queryBlobParams),
+        },
+      });
+
+      return this._retrieveTheaterLayout(response.data);
     } catch (err) {
       const errorResult: ErrorResult = this._resolveRequestError(err);
       return Result.fromFailure(errorResult);
@@ -86,77 +82,64 @@ export class TheaterApiService {
       return Result.failure(`Error from external API host: ${responseBody.error}`, 502);
     }
 
-    const prices: PriceDto[] = responseBody.result.GetPerformanceDetailWithDiscountingExResult.Price
-      .map((p: {zone_no: string, price: string}) => ({
-        zoneId: p.zone_no,
-        price: Number(p.price),
-      }));
+    const prices: PriceDto[] =
+      responseBody.result.GetPerformanceDetailWithDiscountingExResult.Price.map(
+        (p: { zone_no: string; price: string }) => ({
+          zoneId: p.zone_no,
+          price: Number(p.price),
+        }),
+      );
 
     return Result.success(prices);
   }
 
   private _retrieveTheaterLayout(responseBody: any): Result<TheaterLayoutDto> {
-      if (responseBody.error) {
-        return Result.failure(`Error from external API host: ${responseBody.error}`, 502);
-      }
+    if (responseBody.error) {
+      return Result.failure(`Error from external API host: ${responseBody.error}`, 502);
+    }
 
-      const seats: SeatDto[] = responseBody.result.GetSeatsBriefExResults.S
-        .filter((seatWrapper: {D: string}) => !seatWrapper.D.startsWith('0')) 
-        .map((seatWrapper: {D: string}) => seatWrapper.D.split(','))
-        .map((fields: string[]) => ({
-          id: fields[4],
-          statusCode: fields[3],
-          zoneId: fields[5],
-          rowNumber: fields[1],
-          seatNumber: fields[2],
-          sectionId: fields[0],
-        }));
+    const seats: SeatDto[] = responseBody.result.GetSeatsBriefExResults.S.filter(
+      (seatWrapper: { D: string }) => !seatWrapper.D.startsWith('0'),
+    )
+      .map((seatWrapper: { D: string }) => seatWrapper.D.split(','))
+      .map((fields: string[]) => ({
+        id: fields[4],
+        statusCode: fields[3],
+        zoneId: fields[5],
+        rowNumber: fields[1],
+        seatNumber: fields[2],
+        sectionId: fields[0],
+      }));
 
-      const sections: SectionDto[] = responseBody.result.GetSeatsBriefExResults.Section
-        .map((section: {section: string, section_desc: string}) => ({
-          id: section.section,
-          description: section.section_desc,
-        }));
+    const sections: SectionDto[] = responseBody.result.GetSeatsBriefExResults.Section.map(
+      (section: { section: string; section_desc: string }) => ({
+        id: section.section,
+        description: section.section_desc,
+      }),
+    );
 
-      const layout: TheaterLayoutDto = {seats, sections};
+    const layout: TheaterLayoutDto = { seats, sections };
 
-      return Result.success(layout);
+    return Result.success(layout);
   }
 
   private _resolveRequestError(error: any): ErrorResult {
     if (error.response) {
       return {
-        message: `Got bad response from external API with status: "${error.response.status}"`, 
+        message: `Got bad response from external API with status: "${error.response.status}"`,
         code: 502,
       };
-    } 
+    }
     if (error.request) {
       return {
         message: 'Failed to get the response from the external API',
         code: 504,
       };
-    } 
+    }
     return {
       message: 'Something bad happend while setting up the request to API',
       code: 500,
-    }
-  }
-
-  private _getQuery(params: any, paramsToBase64: any): string {
-    if (!params && !paramsToBase64) {
-      return '';
-    }
-    const urlParams = new URLSearchParams();
-
-    for (const [key, value] of Object.entries(params) as [string, any]) {
-      urlParams.append(key, value.toString());
-    }
-
-    for (const [key, value] of Object.entries(paramsToBase64) as [string, any]) {
-      const base64Data = this._toBase64String(value);
-      urlParams.append(key, base64Data);
-    }
-    return urlParams.toString();
+    };
   }
 
   private _toBase64String(obj: any): string {
