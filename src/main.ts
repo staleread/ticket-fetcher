029c19b2b18bb7dd4296430f7857e5fdfa13ce6a
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2024-07-27 18:07:59 +0300

    Replace hard-coded port with the one from .env

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { config as loadEnv } from 'dotenv';
 import { AppModule } from './app.module';
 
 const start = async () => {
+  const port: number = Number(process.env.PORT) || 5000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+
+  await app.listen(port);
 };
 
+loadEnv();
 start();
