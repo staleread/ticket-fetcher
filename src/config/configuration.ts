f75a3a8b9f0f12ee534329a123441f4163ae75a4
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2024-07-27 17:43:09 +0300

    Move sample env config values to .env.example

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,14 +1,12 @@
 export default () => ({
-  port: Number(process.env.PORT) || 5000,
+  port: Number(process.env.PORT),
   theaterApi: {
-    domain: process.env.THEATER_API_DOMAIN || 'ctg-proxy-live.stageblocks.net',
-    timeout: Number(process.env.THEATER_API_TIMEOUT) || 60_000,
+    domain: process.env.THEATER_API_DOMAIN,
+    timeout: Number(process.env.THEATER_API_TIMEOUT),
     loginInfo: {
-      sessionKey:
-        process.env.THEATER_API_SESSION_KEY ||
-        'f71a317a443211efa3150050569fac8400000000000000000000000000000000',
-      sourceNumber: process.env.THEATER_API_SOURCE_NUMBER || '15686',
-      modeOfSale: process.env.THEATER_API_MOS || '6',
+      sessionKey: process.env.THEATER_API_SESSION_KEY,
+      sourceNumber: process.env.THEATER_API_SOURCE_NUMBER,
+      modeOfSale: process.env.THEATER_API_MOS,
     },
   },
 });
